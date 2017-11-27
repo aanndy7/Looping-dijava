57886504d478d54b0950b3be4b3752809bb3f726
Andi Rustandi <32457564+aanndy7@users.noreply.github.com>
2017-11-27 20:57:04 +0700

    Add files via upload

---
+++ LatihanStars.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class latihanStars {
+	public static void main(String[] args){
+	int a;
+		Scanner input = new Scanner(System.in);
+		System.out.println("masukan nilai : ");
+	a=input.nextInt();
+	for(int i=0;i<a;i++){
+		for(int j=0;j<a;j++){
+	System.out.print("2");
+		}
+	System.out.println("");
+		}
+	}
+}
